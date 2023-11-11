c1f9d8d13f573846f7b74669fb6d4e61efaf3c47
sanju <sanju.john@edstem.com>
2023-11-11 11:34:01 +0530

    first commit

---
+++ src/main/java/com/customanalytics/customanalyticsrestapinew/CustomAnalyticsRestApiNewApplication.java
@@ -0,0 +1,13 @@
+package com.customanalytics.customanalyticsrestapinew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomAnalyticsRestApiNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomAnalyticsRestApiNewApplication.class, args);
+	}
+
+}
