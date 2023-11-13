4acc6777babe6d472986363c59dcdb3567096d90
jenu <jenu.s@edstem.com>
2023-11-13 12:24:32 +0530

    Api for getData By index name added

--- src/main/java/com/customanalytics/customanalyticsrestapinew/CustomAnalyticsRestApiNewApplication.java
+++ src/main/java/com/customanalytics/customanalyticsrestapinew/CustomAnalyticsRestApiNewApplication.java
@@ -1,11 +1,5 @@
 package com.customanalytics.customanalyticsrestapinew;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
