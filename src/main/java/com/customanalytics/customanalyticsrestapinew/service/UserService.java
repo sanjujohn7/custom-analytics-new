58426b96d50f9e61e06e56113e26380929a77c56
sanju <sanju.john@edstem.com>
2023-11-17 14:32:15 +0530

    validation for username

--- src/main/java/com/customanalytics/customanalyticsrestapinew/service/UserService.java
+++ src/main/java/com/customanalytics/customanalyticsrestapinew/service/UserService.java
@@ -2,11 +2,14 @@ package com.customanalytics.customanalyticsrestapinew.service;
 
 import com.customanalytics.customanalyticsrestapinew.contract.request.UserRequest;
 import com.customanalytics.customanalyticsrestapinew.contract.response.UserResponse;
+import com.customanalytics.customanalyticsrestapinew.exception.UserNotFoundException;
 import com.customanalytics.customanalyticsrestapinew.model.User;
 import com.customanalytics.customanalyticsrestapinew.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +18,13 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     public UserResponse addUser(UserRequest userRequest) {
+    Optional<User> userOptional = userRepository.findByName(userRequest.getName().trim());
+    if (userOptional.isPresent()){
+    throw new UserNotFoundException("User name already exists !");
+    }
         User user =
                 User.builder()
-                        .name(userRequest.getName())
+                        .name(userRequest.getName().trim())
                         .email(userRequest.getEmail().toLowerCase().trim())
                         .password(passwordEncoder.encode(userRequest.getPassword()))
                         .build();
